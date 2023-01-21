749a111544a148ce9193c8e8c002d72ee2a70bdd
johnmuthua <caveomuthama@gmail.com>
2023-01-21 10:56:34 +0300

    calculate lcm

--- two_sets.js
+++ two_sets.js
@@ -2,6 +2,14 @@ function twoSets(arr) {
   //Get the largest and smallest integers
   min = arr[0] > arr[1] ? arr[1] : arr[0];
   console.log(min);
+  //Get lcd
+  while (true) {
+    if (min % arr[0] === 0 && min % arr[1] === 0) {
+      console.log(`The LCM of ${arr[0]} and ${arr[1]} is ${min}`);
+      break;
+    }
+    min++;
+  }
 }
 
 console.log(twoSets([36, 6]));
